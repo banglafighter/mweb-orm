45ede13336de364c3e66bb15bf0b7006d5eba37c
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-08 18:14:06 +0600

    Exposed Model Classes

--- mweb_orm/__init__.py
+++ mweb_orm/__init__.py
@@ -1,3 +1,5 @@
 from mweb_orm.model.mweb_base_model import MWebBaseModel
 from mweb_orm.model.mweb_other_model import MWebModel
+from mweb_orm.model.mweb_other_model import MWebDatedModel
+from mweb_orm.model.mweb_other_model import MWebIDModel
 from mweb_orm.orm.mweb_orm import mweb_orm
