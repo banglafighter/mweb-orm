d1d1fabd0fe6c3fe62e2d7cc9b9f78f453b0a4a6
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-09 12:26:15 +0600

    Added Save all functionalities

--- mweb_orm/model/mweb_base_model.py
+++ mweb_orm/model/mweb_base_model.py
@@ -1,9 +1,13 @@
+from typing import TypeVar, Type
+from sqlalchemy import inspect
 from mweb_orm.model.mweb_master_model import MWebMasterModel
 from mweb_orm.orm import mweb_orm
 
+T = TypeVar("T", bound="MWebBaseModel")
 
 class MWebBaseModel(MWebMasterModel):
     __abstract__ = True
+    __was_saved = False
 
     def before_save(self):
         """
@@ -18,6 +22,7 @@ class MWebBaseModel(MWebMasterModel):
         return self
 
     async def save(self, commit: bool = True):
+        self.__was_saved = False
         async with await mweb_orm.get_session() as session:
             async with session.begin():
                 self.before_save()
@@ -26,4 +31,27 @@ class MWebBaseModel(MWebMasterModel):
                 self.after_save()
                 if commit:
                     await session.commit()
+                self._set_save_status(self)
         return self
+
+    def _set_save_status(self, model):
+        self.__was_saved = inspect(model).persistent
+
+    def is_saved(self) -> bool:
+        return self.__was_saved
+
+    @classmethod
+    async def save_all(cls: Type[T], models: list[T], commit: bool = True):
+        async with await mweb_orm.get_session() as session:
+            async with session.begin():
+                for model in models:
+                    model.before_save()
+                    session.add(model)
+                await session.flush()
+
+                for model in models:
+                    model.after_save()
+                    model._set_save_status(model)
+
+                if commit:
+                    await session.commit()
