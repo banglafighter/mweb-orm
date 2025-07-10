05df82a7ccccedbfb5db773595e2388234a5ae6e
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-10 16:09:50 +0600

    Updated to SDLizer and None

--- mweb_orm/__init__.py
+++ mweb_orm/__init__.py
@@ -5,4 +5,11 @@ from mweb_orm.model.mweb_other_model import MWebIDModel
 
 from sqlalchemy import and_, or_
 
-__all__ = ["and_", "or_"]
+__all__ = [
+    "and_",
+    "or_",
+    "MWebBaseModel",
+    "MWebDatedModel",
+    "MWebModel",
+    "MWebBaseModel",
+]
--- mweb_orm/common/mweb_orm_data.py
+++ mweb_orm/common/mweb_orm_data.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
+from mw_common import SDLize
 
 
 @dataclass(kw_only=True)
-class Pagination:
+class Pagination(SDLize):
     page: int = 1
     itemPerPage: int = 20
     total: int = 0
     totalPage: int = 0
-    items: list
+    items: list | None = None
 
     def __repr__(self) -> str:
         return f"page: {self.page}, itemPerPage: {self.itemPerPage}, total: {self.total}"
