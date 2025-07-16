bec45d89f5d3c387d43cdf90f8ee2e165c6698da
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-16 09:00:48 +0600

    Rename the CLI

--- mweb_orm/common/mweb_orm_cli.py
+++ mweb_orm/common/mweb_orm_cli.py
@@ -5,7 +5,7 @@ from mweb.engine.mweb_cli import MWebCLI
 from mweb_orm.orm.mweb_orm import mweb_orm
 
 mweb_orm_cli = MWebCLI(name="db", help_text="MWeb Database Manipulation Interface")
-_orm_mweb_app: MWebBase | None = None
+_identity_cli_mweb_app: MWebBase | None = None
 
 
 @mweb_orm_cli.command("init", help="Initialize database models")
@@ -13,7 +13,7 @@ def initialize():
     asyncio.run(_initialize_async())
 
 async def _initialize_async():
-    async with _orm_mweb_app.app_context():
+    async with _identity_cli_mweb_app.app_context():
         await mweb_orm.create_drop_all_model(action="create")
         Console.blue("Successfully Initialized Database & Models", bold=True, system_log=True)
 
@@ -24,13 +24,13 @@ def drop():
 
 
 async def _drop_async():
-    async with _orm_mweb_app.app_context():
+    async with _identity_cli_mweb_app.app_context():
         await mweb_orm.create_drop_all_model(action="drop")
         Console.blue('Successfully Drop Database Models', bold=True, system_log=True)
 
 
 def register_mweb_orm_cli(mweb_app: MWebBase, config: MWebConfig):
-    global _orm_mweb_app
+    global _identity_cli_mweb_app
     _orm_mweb_app = mweb_app
     if mweb_app is not None:
         mweb_app.cli.add_command(mweb_orm_cli)
