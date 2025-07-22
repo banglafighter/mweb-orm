c749c4f0150a3a34b19288e0d63add3504254ad4
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-23 00:29:15 +0600

    Updated relation loading params

--- mweb_orm/orm/mweb_orm_props.py
+++ mweb_orm/orm/mweb_orm_props.py
@@ -102,7 +102,7 @@ class MWebORMProps:
     def ForeignKey(self, column, onupdate: str = None, ondelete: str = None, name=None):
         return ForeignKey(column, onupdate=onupdate, ondelete=ondelete, name=name)
 
-    def Relationship(self, argument, viewonly: bool = False, lazy: LazyLoadType = "select", order_by: str | bool = False, uselist: bool = False, primaryjoin: str = None, remote_side: str = None, backref: str = None, back_populates: str = None):
+    def Relationship(self, argument, viewonly: bool = False, lazy: LazyLoadType = "joined", order_by: str | bool = False, uselist: bool = False, primaryjoin: str = None, remote_side: str = None, backref: str = None, back_populates: str = None):
         return relationship(
             argument=argument, viewonly=viewonly, lazy=lazy, order_by=order_by, uselist=uselist,
             primaryjoin=primaryjoin, remote_side=remote_side, backref=backref, back_populates=back_populates,
