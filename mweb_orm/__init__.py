7edca6c0558a618f35124f8d301db7772e06ce42
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-09-29 12:16:46 +0600

    added agreegation method

--- mweb_orm/__init__.py
+++ mweb_orm/__init__.py
@@ -3,16 +3,18 @@ from mweb_orm.model.mweb_other_model import MWebModel
 from mweb_orm.model.mweb_other_model import MWebDatedModel
 from mweb_orm.model.mweb_other_model import MWebIDModel
 
-from sqlalchemy import and_ as sa_and_, or_ as sa_or_
+from sqlalchemy import and_ as sa_and_, or_ as sa_or_, func as sa_func
 from sqlalchemy.orm import make_transient as sa_make_transient
 
 and_ = sa_and_
 or_ = sa_or_
 make_transient = sa_make_transient
+func = sa_func
 
 __all__ = [
     "and_",
     "or_",
+    "func",
     "make_transient",
     "MWebBaseModel",
     "MWebDatedModel",
