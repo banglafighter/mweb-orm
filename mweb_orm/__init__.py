9fa012f7d5caa71b5ca477b91347fc9a2e9357bd
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-12-14 06:57:25 +0600

    Make Relative Export

--- mweb_orm/__init__.py
+++ mweb_orm/__init__.py
@@ -1,7 +1,7 @@
-from mweb_orm.model.mweb_base_model import MWebBaseModel
-from mweb_orm.model.mweb_other_model import MWebModel
-from mweb_orm.model.mweb_other_model import MWebDatedModel
-from mweb_orm.model.mweb_other_model import MWebIDModel
+from .model.mweb_base_model import MWebBaseModel
+from .model.mweb_other_model import MWebModel
+from .model.mweb_other_model import MWebDatedModel
+from .model.mweb_other_model import MWebIDModel
 
 from sqlalchemy import and_ as sa_and_, or_ as sa_or_, func as sa_func
 from sqlalchemy.orm import make_transient as sa_make_transient
